3f301e062e25e0970c0b137444fe2532647c0999
Jens Tuyls <jtuyls@princeton.edu>
2025-09-11 13:31:13 -0400

    robustify eval script

--- scripts/eval/eval.sh
+++ scripts/eval/eval.sh
@@ -1,16 +1,21 @@
-TASK=countdown-4
+TASK=math
 MODEL_PATH=Qwen/Qwen2.5-7B-Instruct
 RESUME_MODE=resume_path
-RESUME_FROM_PATH='checkpoints/llm-exploration-rl-training/countdown-4_elliptical_seed_41_kl_0.0_ppo_epochs_1_beta_0.01/best_pass@1'
+RESUME_FROM_PATH='checkpoints/llm-exploration-rl-training/math_elliptical_seed_41_kl_0.0_ppo_epochs_1_beta_0.01/best_pass@1'
 CHECKPOINT_SAVE_CONTENTS='["model"]'
+
+# assert TASK is in RESUME_FROM_PATH
+if ! echo "${RESUME_FROM_PATH}" | grep -q "${TASK}"; then
+    echo "ERROR: TASK is not in RESUME_FROM_PATH"
+    exit 1
+fi
 
 echo "Eval job on ${TASK} with the following parameters:"
 echo "MODEL_PATH: ${MODEL_PATH}"
 echo "RESUME_MODE: ${RESUME_MODE}"
 echo "RESUME_FROM_PATH: ${RESUME_FROM_PATH}"
 echo "CHECKPOINT_SAVE_CONTENTS: ${CHECKPOINT_SAVE_CONTENTS}"
-# sbatch --job-name=eval_${TASK} scripts/eval/eval.slurm \
-sh scripts/eval/eval.slurm \
+sbatch --job-name=eval_${TASK} scripts/eval/eval.slurm \
     ${MODEL_PATH} \
     ${RESUME_MODE} \
     "${RESUME_FROM_PATH}" \
