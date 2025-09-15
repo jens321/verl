3d9d0548c0ff3c79e47b5bf2142e5665342118fd
Jens Tuyls <jtuyls@princeton.edu>
2025-09-15 11:12:31 -0400

    update eval script to 1 gpu

--- scripts/eval/eval.sh
+++ scripts/eval/eval.sh
@@ -1,12 +1,15 @@
 TASK=math
-MODEL_PATH=Qwen/Qwen2.5-7B-Instruct
-RESUME_MODE=resume_path
-RESUME_FROM_PATH='/scratch/gpfs/KARTHIKN/jtuyls/checkpoints/llm-exploration-rl-training/math_unlikely_seed_43_kl_0.0/best_pass@1'
+# NAME=math_elliptical_seed_42_kl_0.0_ppo_epochs_1_beta_0.01_turn_off_elliptical_if_none_correct_True_sparse_dim_32
+# NAME=math_grpo_seed_43_kl_0.0_ppo_epochs_1
+NAME=math_unlikely_seed_43_kl_0.0
+MODEL_PATH=/scratch/gpfs/KARTHIKN/jtuyls/checkpoints/llm-exploration-rl-training/${NAME}/best_pass@1/actor/hf #Qwen/Qwen2.5-7B-Instruct
+RESUME_MODE=disable #resume_path
+RESUME_FROM_PATH="" #'/scratch/gpfs/KARTHIKN/jtuyls/checkpoints/llm-exploration-rl-training/math_unlikely_seed_43_kl_0.0/best_pass@1'
 CHECKPOINT_SAVE_CONTENTS='["model"]'
 
 # assert TASK is in RESUME_FROM_PATH
-if ! echo "${RESUME_FROM_PATH}" | grep -q "${TASK}"; then
-    echo "ERROR: TASK is not in RESUME_FROM_PATH"
+if ! echo "${MODEL_PATH}" | grep -q "${TASK}"; then
+    echo "ERROR: TASK is not in MODEL_PATH"
     exit 1
 fi
 
@@ -15,7 +18,7 @@ echo "MODEL_PATH: ${MODEL_PATH}"
 echo "RESUME_MODE: ${RESUME_MODE}"
 echo "RESUME_FROM_PATH: ${RESUME_FROM_PATH}"
 echo "CHECKPOINT_SAVE_CONTENTS: ${CHECKPOINT_SAVE_CONTENTS}"
-sbatch --job-name=eval_${TASK} scripts/eval/eval.slurm \
+sbatch --job-name=eval_${NAME} scripts/eval/eval.slurm \
     ${MODEL_PATH} \
     ${RESUME_MODE} \
     "${RESUME_FROM_PATH}" \
