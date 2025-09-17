70e8a9acfe81ad3e4fc24d7b4860d9cc8cf005af
Jens Tuyls <jtuyls@princeton.edu>
2025-09-17 12:02:44 -0400

    shrink and randomize countdown dataset

--- examples/data_preprocess/count_down_4.py
+++ examples/data_preprocess/count_down_4.py
@@ -8,6 +8,7 @@ from typing import List, Tuple
 from random import randint, seed, choice
 import argparse
 
+import numpy as np
 import datasets
 from tqdm import tqdm
 
@@ -25,7 +26,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--local_dir', default='~/data/countdown-4')
     parser.add_argument('--hdfs_dir', default=None)
-    parser.add_argument('--train_size', type=int, default=8192)
+    parser.add_argument('--train_size', type=int, default=1024) # 8192
     parser.add_argument('--dev_size', type=int, default=512)
     parser.add_argument('--test_size', type=int, default=1024)
 
@@ -35,9 +36,16 @@ if __name__ == '__main__':
     print(f"Loading the {data_source} dataset from huggingface...", flush=True)
     # NOTE: there is only train split, so we use train split as both train/dev/test
     dataset = datasets.load_dataset(data_source, split="train", trust_remote_code=True)
-    train_range = range(args.train_size)
-    dev_range = range(args.train_size, args.train_size + args.dev_size)
-    test_range = range(args.train_size + args.dev_size, args.train_size + args.dev_size + args.test_size)
+
+    # train_range = range(args.train_size)
+    # dev_range = range(args.train_size, args.train_size + args.dev_size)
+    # test_range = range(args.train_size + args.dev_size, args.train_size + args.dev_size + args.test_size)
+
+    all_indices = np.random.permutation(len(dataset))
+    train_range = all_indices[:args.train_size]
+    dev_range = all_indices[args.train_size:args.train_size + args.dev_size]
+    test_range = all_indices[args.train_size + args.dev_size:args.train_size + args.dev_size + args.test_size]
+
     train_dataset = dataset.select(train_range)
     dev_dataset = dataset.select(dev_range)
     test_dataset = dataset.select(test_range)
