0906924fb93d7e222a6394f9269703e41ec3cf04
Jens Tuyls <jtuyls@princeton.edu>
2025-11-04 17:13:11 -0500

    revert

--- verl/workers/reward_manager/__init__.py
+++ verl/workers/reward_manager/__init__.py
@@ -17,8 +17,6 @@ from .batch import BatchRewardManager
 from .dapo import DAPORewardManager
 from .naive import NaiveRewardManager
 from .prime import PrimeRewardManager
-from .elliptical import EllipticalRewardManager
-from .unlikely import UnlikelyRewardManager
 
 # Note(haibin.lin): no need to include all reward managers here in case of complicated dependencies
 __all__ = [
@@ -26,8 +24,6 @@ __all__ = [
     "DAPORewardManager",
     "NaiveRewardManager",
     "PrimeRewardManager",
-    "EllipticalRewardManager",
-    "UnlikelyRewardManager",
     "register",
     "get_reward_manager_cls",
 ]
