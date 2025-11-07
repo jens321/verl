803541c4b9d309344c22b6794cdcf28f5f695eb5
Jens Tuyls <jtuyls@princeton.edu>
2025-11-07 14:17:40 -0500

    simplify model merge script

--- recipe/rep_exp/model_merge.sh
+++ recipe/rep_exp/model_merge.sh
@@ -1,5 +1,4 @@
-NAME=countdown-4_elliptical_seed_44_kl_0.0_ppo_epochs_1_beta_0.01_turn_off_elliptical_if_none_correct_True_sparse_dim_128
-CHECKPOINT_PATH=/scratch/gpfs/jtuyls/llm-rl-exploration/checkpoints/llm-exploration-rl-training/${NAME}/global_step_200/actor
+CHECKPOINT_PATH=${1} # /path/to/global_step_X/actor, where X is the global step of the checkpoint with the best pass@1 on dev
 
 python3 -m verl.model_merger merge \
     --backend fsdp \
